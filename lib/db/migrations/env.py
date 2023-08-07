2a07c89a6d4863ad4ff728a62fa9634433177cbe
Lawrence Nigel Anderson <nigel.lawrenceanderson@gmail.com>
2023-08-07 16:08:25 -0500

    fixes duplicate alembic.ini issue

--- lib/db/migrations/env.py
+++ lib/db/migrations/env.py
@@ -1,5 +1,5 @@
 from logging.config import fileConfig
-
+from lib.db.models import Base
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
@@ -18,8 +18,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
-
+target_metadata = Base.metadata
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
