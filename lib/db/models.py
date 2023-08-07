65eca4a1ef63a1e38e01c042dd99374ff8a94318
Lawrence Nigel Anderson <nigel.lawrenceanderson@gmail.com>
2023-08-07 15:23:41 -0500

    does not change anything lol

--- lib/db/models.py
+++ lib/db/models.py
@@ -1,8 +1,8 @@
 # from sqlalchemy import func
-from sqlalchemy import create_engine, Column, Integer, String, DateTime
+from sqlalchemy import create_engine, Column, Integer, String, DateTime 
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine('sqlite:///recipe.db').
+engine = create_engine('sqlite:///recipe.db')
 
 Base = declarative_base()
 
@@ -10,3 +10,29 @@ class Recipe(Base):
     __tablename__ = "recipes"
 
     id = Column(Integer(), primary_key=True)
+    name = Column(String())
+    genre = Column(String())
+    time = Column(String())
+    
+    # users = relationship('User')
+    # ingredients = relationship('Ingredient')
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
+    password = Column(String())
+    food_restrictions = Column(String())
+    saved_recipes = Column(String())
+
+
+class Ingredient(Base):
+    __tablename__ = "ingredients"
+
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
+    category = Column(String())
+
+    # recipe used in
+    # recipe = relationship()
